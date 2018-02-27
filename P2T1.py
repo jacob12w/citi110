a91500039d21a9db5fafc3982542aab804398551
jacob12w <36903160+jacob12w@users.noreply.github.com>
2018-02-27 18:24:00 -0500

    Add files via upload
    first submission

---
+++ P2T1.py
@@ -0,0 +1,12 @@
+#CTI-110
+#P2T1 - Sales Prediction
+#Jacob Williams
+#Feb. 15, 2018
+
+#Calculate projected sales
+
+total_sales=float(input("Enter the projected sales: "))
+
+profit=total_sales * 0.23
+
+print("The profit is $", profit)
